82b698be6e05e276c34d187ff656342c30c5b6e0
Pedro <pedrowindmoller@gmail.com>
2024-11-13 18:47:17 -0300

    Project created

---
+++ theboys/src/main/java/paradigmas/theboys/TheboysApplication.java
@@ -0,0 +1,13 @@
+package paradigmas.theboys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheboysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheboysApplication.class, args);
+	}
+
+}
