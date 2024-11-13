599f90bdc1dbc591a465f25aa0e67ad330052b94
Pedro <pedrowindmoller@gmail.com>
2024-11-13 18:53:15 -0300

    project created

---
+++ src/test/java/paradigmas/theboys/TheboysApplicationTests.java
@@ -0,0 +1,13 @@
+package paradigmas.theboys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheboysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
