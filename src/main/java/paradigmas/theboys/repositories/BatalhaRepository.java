8e3955dd35040a2d747ace7ddfbe4b72f9c20953
Pedro <pedrowindmoller@gmail.com>
2025-06-03 21:53:17 -0300

    .

--- src/main/java/paradigmas/theboys/repositories/BatalhaRepository.java
+++
@@ -1,7 +0,0 @@
-package paradigmas.theboys.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import paradigmas.theboys.entities.Batalha;
-
-public interface BatalhaRepository extends JpaRepository<Batalha, Integer> {
-}
