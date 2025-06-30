2c08c77eb043567cad5d720b47df90ebfbe36787
Pedro <pedrowindmoller@gmail.com>
2025-06-30 11:20:09 -0300

    removendo lombok

--- aula/aula/src/main/java/com/devsuperior/aula/entities/Order.java
+++ aula/aula/src/main/java/com/devsuperior/aula/entities/Order.java
@@ -1,16 +1,35 @@
 package com.devsuperior.aula.entities;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import java.util.Random;
 
-@Getter
-@Setter
 public class Order {
     private Integer code;
     private Double basic;
     private Integer discount;
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public Integer getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Integer discount) {
+        this.discount = discount;
+    }
+
+    public Double getBasic() {
+        return basic;
+    }
+
+    public void setBasic(Double basic) {
+        this.basic = basic;
+    }
 
     public Order() {
         Random random = new Random();
