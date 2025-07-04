c1ec7ef8f5dce26b5f898ee28fb5a9eb99114b1e
Pedro Windmoller <142617499+pedrowind@users.noreply.github.com>
2025-07-03 21:26:23 -0300

    Update AulaApplication.java

--- aula/aula/src/main/java/com/devsuperior/aula/AulaApplication.java
+++ aula/aula/src/main/java/com/devsuperior/aula/AulaApplication.java
@@ -23,16 +23,16 @@ public class AulaApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) {
-		Order order1 = new Order(1034, 150.0, 20.0);
+		Order order1 = new Order(1034, 150.0, 20);
 		System.out.println("Pedido código " + order1.getCode());
-		System.out.println(Valor total: R$ " + orderService.total(order1));
+		System.out.println("Valor total: R$ " + orderService.total(order1));
 
-		Order order2 = new Order(2282, 800.0, 10.0);
-		System.out.println("Pedido código " + order1.getCode());
-		System.out.println(Valor total: R$ " + orderService.total(order1));
+		Order order2 = new Order(2282, 800.0, 10);
+		System.out.println("Pedido código " + order2.getCode());
+		System.out.println("Valor total: R$ " + orderService.total(order2));
 
-		Order order1 = new Order(1309, 95.90, 0.0);
-		System.out.println("Pedido código " + order1.getCode());
-		System.out.println(Valor total: R$ " + orderService.total(order1));
+		Order order3 = new Order(1309, 95.90, 0);
+		System.out.println("Pedido código " + order3.getCode());
+		System.out.println("Valor total: R$ " + orderService.total(order3));
 	}
 }
